0e4f14f63c087f5e5d4fe8b061e423bf7d600dab
bucarjeva <jb2662@student.uni-lj.si>
2023-08-20 19:47:28 +0200

    edits .desktop file and removes broken link

--- discord_update.sh
+++ discord_update.sh
@@ -11,9 +11,15 @@ sudo mv /opt/Discord /opt/discord
 
 sudo chmod u+x /opt/discord/discord.desktop
 
+# to launch from terminal
 sudo ln -sf -v /opt/discord/Discord /usr/bin/discord
 
+sudo sed -i '/Exec/d;/Icon/d' /opt/discord/discord.desktop
+sudo bash -c "cat <<< 'Exec=/opt/discord/Discord' >> /opt/discord/discord.desktop"
+sudo bash -c "cat <<< 'Icon=/opt/discord/discord.png' >> /opt/discord/discord.desktop"
+
 # to launch from menu
+sudo rm /usr/share/applications/discord.desktop
 sudo cp -r -v /opt/discord/discord.desktop /usr/share/applications
 
 exit 0
