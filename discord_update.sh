c50171ec74f170a695e749cd18f8017c4af0c0c4
bucarjeva <jb2662@student.uni-lj.si>
2024-10-25 20:13:52 +0200

    dont need update file as argument

--- discord_update.sh
+++ discord_update.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-f=$1
-echo "$f"
+echo "Home directory (using ~): $HOME"
+echo "Home directory (using $HOME variable): $(echo "$HOME")"
+# path to discord-x.x.xx.tar.gz
+file_path="$HOME/Downloads"
 
-sudo rm -rf /opt/discord
+# get latest discord update file from file_path
+update_file=$(ls -1 -t $file_path | grep discord | head -n1)
 
-sudo tar -xvzf $1 -C /opt
+echo $update_file
 
-sudo mv /opt/Discord /opt/discord
+sudo rm -rf /opt/discord &&
 
-sudo chmod u+x /opt/discord/discord.desktop
+sudo tar -xvzf $update_file -C /opt &&
+
+sudo mv /opt/Discord /opt/discord &&
+
+sudo chmod u+x /opt/discord/discord.desktop &&
 
 # to launch from terminal
-sudo ln -sf -v /opt/discord/Discord /usr/bin/discord
+sudo ln -sf -v /opt/discord/Discord /usr/bin/discord &&
 
-sudo sed -i '/Exec/d;/Icon/d' /opt/discord/discord.desktop
-sudo bash -c "cat <<< 'Exec=/opt/discord/Discord' >> /opt/discord/discord.desktop"
-sudo bash -c "cat <<< 'Icon=/opt/discord/discord.png' >> /opt/discord/discord.desktop"
+sudo sed -i '/Exec/d;/Icon/d' /opt/discord/discord.desktop &&
+sudo bash -c "cat <<< 'Exec=/opt/discord/Discord' >> /opt/discord/discord.desktop" &&
+sudo bash -c "cat <<< 'Icon=/opt/discord/discord.png' >> /opt/discord/discord.desktop" &&
 
 # to launch from menu
-sudo rm /usr/share/applications/discord.desktop
+sudo rm /usr/share/applications/discord.desktop &&
 sudo cp -r -v /opt/discord/discord.desktop /usr/share/applications
 
 exit 0
